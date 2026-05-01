eb26b35fb3ee37d24e3562ad38cfb0c8ef593d07
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2026-05-01 18:05:05 +0300

    Initial commit: FastAPI basic setup

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from urllib import request
+from urllib.request import Request
+
+from fastapi import FastAPI, Request, Form
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
@@ -11,3 +17,23 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/test_form_data/{user_id}")
+def test_form_data(request: Request, user_id: int, query_args1: str| None = None , query_args2: str| None = None):
+    return templates.TemplateResponse(request,
+                                      "item.html",
+                                      context = {"user_id": user_id, "query_args1": query_args1, "query_args2": query_args2}
+                                      )
+
+
+
+class FormData(BaseModel):
+    first_name: str
+    last_name: str
+
+
+@app.post("/test_form_data")
+def test_form_data(form_data: FormData):
+    return {"message": f"Hello World {form_data.first_name} {form_data.last_name}"}
+
